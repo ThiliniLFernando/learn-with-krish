2250bdb061d1266fda12c82620ae71a1bc905187
ThiliniLFernando <fthilini34@gmail.com>
2023-05-18 13:04:38 +0530

    update rabbitmq

---
+++ rabbitmq/Producer.java
@@ -0,0 +1,29 @@
+package org.thilini.example;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    private final static String QUEUE_NAME = "hello";
+    public static void main(String[] argv){
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
